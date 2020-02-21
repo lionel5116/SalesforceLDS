9bb4b27f068938be788a7002041f89df243df654
lionel5116 <lioneljones5116@gmail.com>
2020-02-21 14:05:37 -0600

    Added Javascript

--- force-app/main/default/aura/WebSearch/WebSearchHelper.js
+++ force-app/main/default/aura/WebSearch/WebSearchHelper.js
@@ -1,5 +1,6 @@
 ({
-    helperMethod : function() {
-
+    setSearchTerm : function(component, event) {
+      var searchTerm = component.find('searchBox').getElement().value;
+      component.set("v.searchTerm",searchTerm);
     }
 })
