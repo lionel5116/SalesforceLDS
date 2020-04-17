219a32f0e8ce16d53ee4af70de0edcfbf5909f16
lionel5116 <lioneljones5116@gmail.com>
2020-04-16 22:35:34 -0500

    LWC Stuff - last 1035 - 4162020

--- force-app/main/default/lwc/incomeApproachLWC/incomeApproachLWC.js
+++ force-app/main/default/lwc/incomeApproachLWC/incomeApproachLWC.js
@@ -1,76 +1,132 @@
 import { LightningElement , api, track} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import NAME_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Name';
+import ADDRESS_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Address__c';
+import ACCOUNT_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Account_Number__c';
+import PGI_FIELD from '@salesforce/schema/Income_Approach_JobS__c.PGI__c';
+import OCC_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Occupancy__c';
+import NOI_FIELD from '@salesforce/schema/Income_Approach_JobS__c.NOI__c';
+import FIXED_EXP_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Fixed_Expenses__c';
+import VARIABLE_EXP_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Variable_Expenses__c';
+import EGI_FIELD from '@salesforce/schema/Income_Approach_JobS__c.EGI__c';
+import CAP_RATE_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Cap_Rate__c';
+import VALUE_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Value__c';
+import NOTES_FIELD from '@salesforce/schema/Income_Approach_JobS__c.Notes__c';
+import createIncApproachRecord from '@salesforce/apex/CreateIncomeApproachRec.createIncApproachRecord';
 
 export default class IncomeApproachLWC extends LightningElement {
-    
-    @api Income_Approach_JobS__c;
-
-    @track vName = '';
-    @track vAAdress = '';
-    @track vAccountNuber = '';
-    @track vPGI = '';
-    @track vOCC = '';
-    @track vNOI = '';
-    @track vFixedExp = '';
-    @track vVariable = '';
-    @track vEGI = '';
-    @track vCAPRate = '';
-    @track vValue = '';
-    @track vNotes = '';
-    
    
+   @track vName = NAME_FIELD;
+   @track vAAdress = ADDRESS_FIELD;
+   @track vAccountNuber = ACCOUNT_FIELD;
+   @track vPGI = PGI_FIELD;
+   @track vOCC = OCC_FIELD;
+   @track vNOI = NOI_FIELD;
+   @track vFixedExp = FIXED_EXP_FIELD;
+   @track vVariable = VARIABLE_EXP_FIELD;
+   @track vEGI = EGI_FIELD;
+   @track vCAPRate = CAP_RATE_FIELD;
+   @track vValue = VALUE_FIELD;
+   @track vNotes = NOTES_FIELD;
 
+
+   rec = {
+     Name : this.vName,
+     Address__c : this.vAAdress,
+     Account_Number__c : this.vAccountNuber,
+     PGI__c : this.vPGI,
+     Occupancy__c : this.vOCC,
+     NOI__c : this.vNOI,
+     Fixed_Expenses__c : this.vFixedExp,
+     Variable_Expenses__c : this.vVariable,
+     EGI__c : this.vEGI,
+     Cap_Rate__c : this.vCAPRate,
+     Value__c : this.vValue,
+     Notes__c : this.vNotes,
+   }
 
     nameHandler(event)
     {
-        this.vName = event.target.value; 
+        this.rec.Name = event.target.value; 
     }
     addressHandler(event)
     {
-        this.vAAdress = event.target.value; 
+        this.rec.Address__c = event.target.value; 
     }
     accntNumberHandler(event)
     {
-        this.vAccountNuber = event.target.value; 
+        this.rec.Account_Number__c = event.target.value; 
     }
     PGIHandler(event)
     {
-        this.vPGI = event.target.value; 
+        this.rec.PGI__c = event.target.value; 
     }
     OCCHandler(event)
     {
-        this.vOCC = event.target.value; 
+        this.rec.Occupancy__c = event.target.value; 
     }
     NOIHandler(event)
     {
-        this.vNOI = event.target.value; 
+        this.rec.NOI__c = event.target.value; 
     }
     fixedExpHandler(event)
     {
-        this.vFixedExp = event.target.value; 
+        this.rec.Fixed_Expenses__c = event.target.value; 
     }
     variableExpHandler(event)
     {
-        this.vVariable = event.target.value; 
+        this.rec.Variable_Expenses__c = event.target.value; 
     }
     EGIHandler(event)
     {
-        this.vEGI = event.target.value; 
+        this.rec.EGI__c = event.target.value; 
     }
     capRateHandler(event)
     {
-        this.vCAPRate = event.target.value; 
+        this.rec.Cap_Rate__c = event.target.value; 
     }
     ttlValHandler(event)
     {
-        this.vValue = event.target.value; 
+        this.rec.Value__c = event.target.value;  
     }
     notesHandler(event)
     {
-        this.vNotes = event.target.value; 
+        this.rec.Notes__c = event.target.value; 
     }
 
    
+    handleClick() {
+        createIncApproachRecord({ incRec : this.rec })
+            .then(result => {
+                this.message = result;
+                this.error = undefined;
+                if(this.message !== undefined) {
+                  
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Success',
+                            message: 'Income Approach Record created',
+                            variant: 'success',
+                        }),
+                    );
+                }
+                
+                console.log(JSON.stringify(result));
+                console.log("result", this.message);
+            })
+            .catch(error => {
+                this.message = undefined;
+                this.error = error;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: error.body.message,
+                        variant: 'error',
+                    }),
+                );
+                console.log("error", JSON.stringify(this.error));
+            });
+    }
 
     handleSuccess(event) {
         this.dispatchEvent(
@@ -83,32 +139,33 @@ export default class IncomeApproachLWC extends LightningElement {
     }
 
     clearForm(event) {
-       this.vName = '';
-       this.vPGI = '';
-       this.vAAdress = '';
-       this.vAccountNuber = '';
-       this.vPGI = '';
-       this.vOCC = '';
-       this.vNOI = '';
-       this.vFixedExp = '';
-       this.vVariable = '';
-       this.vEGI = '';
-       this.vCAPRate = '';
-       this.vValue = '';
-       this.vNotes = '';
+
+       this.rec.Name = '';
+       this.rec.Address__c  = ''; 
+       this.rec.Account_Number__c  = '';
+       this.rec.PGI__c  = '';
+       this.rec.Occupancy__c  = '';
+       this.rec.NOI__c  = ''; 
+       this.rec.Fixed_Expenses__c  = ''; 
+       this.rec.Variable_Expenses__c  = ''; 
+       this.rec.EGI__c  = ''; 
+       this.rec.Cap_Rate__c  = ''; 
+       this.rec.Value__c  = '';
+       this.rec.Notes__c  = ''; 
     }
 
     calc(event)
     {
         var totalExpenses;
         console.log("Something is happening");
-        
-        this.vNOI = (this.vPGI * this.vOCC);
-        console.log("NOI: " + this.vNOI);
-        this.totalExpenses = parseFloat (this.vFixedExp) + parseFloat(this.vVariable);
+        this.rec.NOI__c = (this.rec.PGI__c * this.rec.Occupancy__c);
+        console.log("NOI: " + this.rec.NOI__c);
+        this.totalExpenses = parseFloat (this.rec.Fixed_Expenses__c) + parseFloat(this.rec.Variable_Expenses__c);
         console.log("Total Expenses: " + this.totalExpenses);
-        this.vEGI = parseFloat(this.vNOI) - this.totalExpenses;
-        this.vValue = this.vEGI / parseFloat(this.vCAPRate);
+        this.rec.EGI__c = parseFloat(this.rec.NOI__c) - this.totalExpenses;
+        var formattedFinalValue = this.rec.EGI__c / parseFloat(this.rec.Cap_Rate__c);
+        this.rec.Value__c = formattedFinalValue.toFixed(2);
+        console.log(this.rec)
     }
 
 }
